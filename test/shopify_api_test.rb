9d692f0634121b3b695215102bc354b8678431b1
Dennis Theisen <dennis@jadedpixel.com>
2009-06-15 10:34:01 -0400

    Added a simple test and the first entry in the changelog.

--- test/shopify_api_test.rb
+++ test/shopify_api_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ShopifyApiTest < Test::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def setup
+    @session_no_token = ShopifyAPI::Session.new("testshop.myshopify.com")
+    @session = ShopifyAPI::Session.new("testshop.myshopify.com", "any-token")
   end
+  
+  
+  def test_session_not_valid_without_token
+    assert_not @session_no_token.valid?
+  end
+  
+  def test_session_is_valid_with_any_token
+    assert @session.valid?
+  end
+  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,5 +5,12 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'shopify_api'
 
+# setup ShopifyAPI with fake api_key and secret
+ShopifyAPI::Session.setup(:api_key => "API Test key", :secret => "API Test secret")
+
 class Test::Unit::TestCase
+  # Custom Assertions
+  def assert_not(expression)
+    assert_block("Expected <#{expression}> to be false!") { not expression }
+  end  
 end
