334563da348aeaeeaf13178a12aa9aa197bb5691
Dennis Theisen <dennis@jadedpixel.com>
2009-06-16 17:00:28 -0400

    Added custom minimal should method to tests.

--- test/shopify_api_test.rb
+++ test/shopify_api_test.rb
@@ -3,19 +3,19 @@ require 'test_helper'
 class ShopifyApiTest < Test::Unit::TestCase
   
   context "Session" do
-    test "should raise error when blank shop url is provided" do
+    should "raise error when blank shop url is provided" do
       assert_raise(ArgumentError) { ShopifyAPI::Session.new("") }
     end
 
-    test "should not be valid without token" do
+    should "not be valid without token" do
       session = ShopifyAPI::Session.new("testshop.myshopify.com")
       assert_not session.valid?
     end
 
-    test "should be valid with any token" do
+    should "be valid with any token" do
       session = ShopifyAPI::Session.new("testshop.myshopify.com", "any-token")
       assert session.valid?
     end
-    
   end
+  
 end
