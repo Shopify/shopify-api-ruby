a729c91b2b981878b6f668abc7be98e53d2237a1
Dennis Theisen <dennis@shopify.com>
2010-11-22 15:41:34 -0500

    Fix Variant to support accessing both nested variants with a product prefix as well as top level variants directly by variant_id.

--- lib/shopify_api.rb
+++ lib/shopify_api.rb
@@ -302,6 +302,10 @@ module ShopifyAPI
   
   class Variant < Base
     self.prefix = "/admin/products/:product_id/"
+    
+    def self.prefix(options={})
+      options[:product_id].nil? ? "/admin/" : "/admin/products/#{options[:product_id]}/"
+    end
   end
   
   class Image < Base
