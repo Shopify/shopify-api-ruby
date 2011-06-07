734902434d3ab1506e4ae1de2b4dad12e3c5054e
Edward Ocampo-Gooding <edward@edwardog.net>
2011-06-07 16:21:12 -0400

    Added Article#comments

--- lib/shopify_api.rb
+++ lib/shopify_api.rb
@@ -352,6 +352,10 @@ module ShopifyAPI
   
   class Article < Base
     self.prefix = "/admin/blogs/:blog_id/"
+    
+    def comments
+      Comment.find(:all, :param => { :article_id => id })
+    end
   end
   
   class Metafield < Base
