ee105b338fa753f4d1ddaa6a86a5aab25dde8b30
Travis Haynes <travis.j.haynes@gmail.com>
2011-08-12 09:43:33 -0700

    Added pending, cancelled, accepted and declined to RecurringApplicationCharge

--- lib/shopify_api/recurring_application_charge.rb
+++ lib/shopify_api/recurring_application_charge.rb
@@ -1,9 +1,27 @@
 module ShopifyAPI
   class RecurringApplicationCharge < Base
     undef_method :test
-
-    def self.current
-      find(:all).find{|charge| charge.status == 'active'}
+    
+    class << self
+      def current
+        all.find { |c| c.status == 'active' }
+      end
+      
+      def pending
+        all.find { |c| c.status == 'pending' }
+      end
+      
+      def cancelled
+        all.find { |c| c.status == 'cancelled' }
+      end
+      
+      def accepted
+        all.find { |c| c.status == 'accepted' }
+      end
+      
+      def declined
+        all.find { |c| c.status == 'declined' }
+      end
     end
     
     def cancel
