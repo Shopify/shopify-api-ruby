05bc29f610cb8fd1f5ed14393f87536d1032c364
Denis Odorcic <denis.odorcic@jadedpixel.com>
2011-08-31 15:22:04 -0400

    Remove Comment#ham

--- lib/shopify_api/resources/comment.rb
+++ lib/shopify_api/resources/comment.rb
@@ -1,7 +1,6 @@
 module ShopifyAPI
   class Comment < Base
     def remove;   load_attributes_from_response(post(:remove, {}, only_id)); end
-    def ham;      load_attributes_from_response(post(:ham, {}, only_id)); end
     def spam;     load_attributes_from_response(post(:spam, {}, only_id)); end
     def approve;  load_attributes_from_response(post(:approve, {}, only_id)); end
     def restore;  load_attributes_from_response(post(:restore, {}, only_id)); end
