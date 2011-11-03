82feb551c2d1515d73a68270bd08629de1651cb2
Denis Odorcic <denis.odorcic@jadedpixel.com>
2011-11-03 14:12:04 -0400

    Add Fulfillment#cancel

--- lib/shopify_api/resources/fulfillment.rb
+++ lib/shopify_api/resources/fulfillment.rb
@@ -1,5 +1,7 @@
 module ShopifyAPI
   class Fulfillment < Base
     self.prefix = "/admin/orders/:order_id/"
+
+    def cancel; load_attributes_from_response(post(:cancel, {}, only_id)); end
   end
 end
