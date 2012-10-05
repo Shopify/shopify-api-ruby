0769cdd5ec62e295c6dc5e700ded872bc4f856c4
Gareth du Plooy <gduplooy@gmail.com>
2012-10-05 13:35:16 -0500

    Revert "Merge branch 'master' of github.com:Shopify/shopify_api"
    This reverts commit 855fe5d8f95bb7f4361f2c5b0d85e9ab1710aebd, reversing
    changes made to cbe5b167977e32e2793fd4db83796b5e411028c2.

--- lib/shopify_api/json_format.rb
+++ lib/shopify_api/json_format.rb
@@ -1,14 +1,37 @@
 module ActiveResource
-  if ActiveResource::VERSION::STRING =~ /^3\.0\./
-    module Formats
-      module JsonFormat
-        def decode(json)
-          data = ActiveSupport::JSON.decode(json)
-          if data.is_a?(Hash) && data.keys.size == 1
-            data.values.first
-          else
-            data
-          end
+  class Base
+    def encode(options = {})
+      same = dup
+      same.attributes = {self.class.element_name => same.attributes} if self.class.format.extension == 'json'
+      
+      same.send("to_#{self.class.format.extension}", options)
+    end
+  end
+
+  module Formats
+    module JsonFormat
+      def decode(json)
+        data = ActiveSupport::JSON.decode(json)
+        if data.is_a?(Hash) && data.keys.size == 1
+          data.values.first
+        else
+          data
+        end
+      end
+    end
+  end
+end
+
+module ActiveModel
+  module Serializers
+    module JSON
+      def as_json(options = nil)
+        root = options[:root] if options.try(:key?, :root)
+        if include_root_in_json
+          root = self.class.model_name.element if root == true
+          { root => serializable_hash(options) }
+        else
+          serializable_hash(options)
         end
       end
     end
