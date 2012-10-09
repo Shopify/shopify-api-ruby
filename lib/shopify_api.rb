93df6d73596adbb1a93ab79449011aa505a4c20f
James MacAulay <james@shopify.com>
2012-10-09 17:29:23 -0400

    WIP [UNSTAGED]

--- lib/shopify_api.rb
+++ lib/shopify_api.rb
@@ -18,41 +18,6 @@ module ShopifyAPI
     end
   end
   
-  class Base < ActiveResource::Base
-    extend Countable
-    self.include_root_in_json = false
-    self.headers['User-Agent'] = ["ShopifyAPI/#{ShopifyAPI::VERSION}",
-                                  "ActiveResource/#{ActiveResource::VERSION::STRING}",
-                                  "Ruby/#{RUBY_VERSION}"].join(' ')
-
-    class << self
-      def headers
-        if defined?(@headers)
-          @headers
-        elsif superclass != Object && superclass.headers
-          superclass.headers
-        else
-          @headers ||= {}
-        end
-      end
-
-      def activate_session(session)
-        self.site = session.site
-        self.headers.merge!('X-Shopify-Access-Token' => session.token)
-      end
-
-      def clear_session
-        self.site = nil
-        self.headers.delete('X-Shopify-Access-Token')
-      end
-    end                  
-
-    private
-    def only_id
-      encode(:only => :id, :include => [], :methods => [])
-    end
-  end
-  
   ignore_files = ['cli.rb']
   Dir[File.join(File.dirname(__FILE__), 'shopify_api', '**/*.rb')].each do |file|
     require file unless ignore_files.include?(File.basename(file))
---
+++ lib/shopify_api/base.rb
@@ -0,0 +1,36 @@
+module ShopifyAPI
+  class Base < ActiveResource::Base
+    extend Countable
+    self.include_root_in_json = false
+    self.headers['User-Agent'] = ["ShopifyAPI/#{ShopifyAPI::VERSION}",
+                                  "ActiveResource/#{ActiveResource::VERSION::STRING}",
+                                  "Ruby/#{RUBY_VERSION}"].join(' ')
+
+    class << self
+      def headers
+        if defined?(@headers)
+          @headers
+        elsif superclass != Object && superclass.headers
+          superclass.headers
+        else
+          @headers ||= {}
+        end
+      end
+
+      def activate_session(session)
+        self.site = session.site
+        self.headers.merge!('X-Shopify-Access-Token' => session.token)
+      end
+
+      def clear_session
+        self.site = nil
+        self.headers.delete('X-Shopify-Access-Token')
+      end
+    end                  
+
+    private
+    def only_id
+      encode(:only => :id, :include => [], :methods => [])
+    end
+  end
+end
