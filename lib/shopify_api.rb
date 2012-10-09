b0760da1a806049950b36b002c515ca5b0444c76
James MacAulay <james@shopify.com>
2012-10-09 17:35:40 -0400

    require order

--- lib/shopify_api.rb
+++ lib/shopify_api.rb
@@ -17,20 +17,21 @@ module ShopifyAPI
       Integer(get(:count, options))
     end
   end
+end
+
+require 'shopify_api/base'
   
-  ignore_files = ['cli.rb']
-  Dir[File.join(File.dirname(__FILE__), 'shopify_api', '**/*.rb')].each do |file|
-    require file unless ignore_files.include?(File.basename(file))
-  end
-  
-  # Include Metafields module in all enabled classes
-  METAFIELD_ENABLED_CLASSES.each do |klass|
-    "ShopifyAPI::#{klass}".constantize.send(:include, Metafields)
-  end
-  
-  # Include Events module in all enabled classes
-  EVENT_ENABLED_CLASSES.each do |klass|
-    "ShopifyAPI::#{klass}".constantize.send(:include, Events)
-  end
-  
+ignore_files = ['cli.rb']
+Dir[File.join(File.dirname(__FILE__), 'shopify_api', '**/*.rb')].each do |file|
+  require file unless ignore_files.include?(File.basename(file))
+end
+
+# Include Metafields module in all enabled classes
+ShopifyAPI::METAFIELD_ENABLED_CLASSES.each do |klass|
+  "ShopifyAPI::#{klass}".constantize.send(:include, ShopifyAPI::Metafields)
+end
+
+# Include Events module in all enabled classes
+ShopifyAPI::EVENT_ENABLED_CLASSES.each do |klass|
+  "ShopifyAPI::#{klass}".constantize.send(:include, ShopifyAPI::Events)
 end
