8f3203efed64d00004be45f80d0d068197862919
Eduardo Del Balso <e.delbalso@gmail.com>
2013-01-02 13:17:27 -0500

    Allows search queries on Customer resource

--- lib/shopify_api/resources/customer.rb
+++ lib/shopify_api/resources/customer.rb
@@ -1,5 +1,9 @@
 module ShopifyAPI
   class Customer < Base
     include Metafields
+
+    def self.search(query_string)
+      self.find(:all, from: '/admin/customers/search.json',params: {query: query_string})
+    end
   end
 end
