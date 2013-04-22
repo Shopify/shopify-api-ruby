268fd0ef9540b744fd59e7025b8d8ef5d7d01c63
Lydia Krupp-Hunter <lydia@shopify.com>
2013-04-21 20:14:14 -0400

    Clean up blog tests
    Consistancy in fixtures/fix the test

--- test/blog_test.rb
+++ test/blog_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 class BlogTest < Test::Unit::TestCase
   test "blog creation" do
     fake "blogs", :method => :post, :status => 202
-    ShopifyAPI::Blog.create(:title => "Test Blog")
-    assert_equal '{"blog":{"title":"Test Blog"}}', FakeWeb.last_request.body
+    blog = ShopifyAPI::Blog.new(:title => "Test Blog")
+    assert_equal "Test Blog", blog.title
   end
 end
