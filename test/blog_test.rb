2ae5609df4d7018d41e8f51cec3b353392095cf4
Lydia Krupp-Hunter <lydia@shopify.com>
2013-04-21 20:53:19 -0400

    Fix blog test
    L2fakeweb.
    @jduff this should address the comment you had.

--- test/blog_test.rb
+++ test/blog_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 class BlogTest < Test::Unit::TestCase
   test "blog creation" do
-    fake "blogs", :method => :post, :status => 202
-    blog = ShopifyAPI::Blog.new(:title => "Test Blog")
-    assert_equal "Test Blog", blog.title
+    fake "blogs", :method => :post, :status => 202, :body => load_fixture('blog')
+    blog = ShopifyAPI::Blog.create(:title => "Test Blog")
+    assert_equal '{"blog":{"title":"Test Blog"}}', FakeWeb.last_request.body
   end
 end
