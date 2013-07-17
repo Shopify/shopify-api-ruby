ac5dc43ab62e744cc8710504407560a748de40e7
Peter McCracken <peter.mccracken@jadedpixel.com>
2013-07-17 14:52:34 -0400

    update backported version of ActiveRecord::Base.build

--- lib/active_resource/base_ext.rb
+++ lib/active_resource/base_ext.rb
@@ -1,7 +1,7 @@
 module ActiveResource
   class Base
     def self.build(attributes = {})
-      attrs = self.format.decode(connection.get("#{new_element_path}", headers).body).merge(attributes)
+      attrs = self.format.decode(connection.get("#{new_element_path(attributes)}", headers).body).merge(attributes)
       self.new(attrs)
     end
   end
