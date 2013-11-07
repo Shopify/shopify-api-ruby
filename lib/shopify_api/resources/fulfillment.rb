0e75538f3d0d8abd7413b11d2476ffaa12e0f609
Maarten van Grootel <maarten.vangrootel@shopify.com>
2013-11-07 09:48:59 -0500

    Add complete action for Fulfillment endpoint

--- lib/shopify_api/resources/fulfillment.rb
+++ lib/shopify_api/resources/fulfillment.rb
@@ -3,5 +3,6 @@ module ShopifyAPI
     self.prefix = "/admin/orders/:order_id/"
 
     def cancel; load_attributes_from_response(post(:cancel, {}, only_id)); end
+    def complete; load_attributes_from_response(post(:complete, {}, only_id)); end
   end
 end
