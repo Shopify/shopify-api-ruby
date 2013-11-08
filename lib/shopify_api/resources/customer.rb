48d4f54f21783b1bcc26bd78426f971cca1173d1
Neil Costford <costford@gmail.com>
2013-11-08 10:19:39 -0500

    exposes customer#orders

--- lib/shopify_api/resources/customer.rb
+++ lib/shopify_api/resources/customer.rb
@@ -2,6 +2,8 @@ module ShopifyAPI
   class Customer < Base
     include Metafields
 
+    def orders(options = {}); load_attributes_from_response(get(:orders, options)); end
+    
     class DefaultAddress < Base
     end
   end
