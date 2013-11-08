519325ec814b3af5b868a333760554a9955d7f30
Neil Costford <costford@gmail.com>
2013-11-08 12:06:26 -0500

    just use Order.find instead of load_attributes_from_response

--- lib/shopify_api/resources/customer.rb
+++ lib/shopify_api/resources/customer.rb
@@ -2,9 +2,9 @@ module ShopifyAPI
   class Customer < Base
     include Metafields
 
-    def orders(options = {}); load_attributes_from_response(get(:orders, options)); end
-    
-    class DefaultAddress < Base
+    def orders
+      Order.find(:all, :params => {:customer_id => self.id})
     end
+
   end
 end
