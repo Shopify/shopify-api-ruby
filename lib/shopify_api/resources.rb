dbd5698794a111d3708b41213db22604c29725a7
Simon Eskildsen <sirup@sirupsen.com>
2014-01-08 10:48:56 -0500

    resources: fix odd require not working on 2.1

--- lib/shopify_api/resources.rb
+++ lib/shopify_api/resources.rb
@@ -1,2 +1,2 @@
 require 'shopify_api/resources/base'
-Dir.glob "#{File.dirname(__FILE__)}/resources/*", &method(:require)
+Dir.glob("#{File.dirname(__FILE__)}/resources/*").each { |file| require(file) } 
