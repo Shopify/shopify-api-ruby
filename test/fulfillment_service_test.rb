fa337b3c19bf42b94adcf01e38e65e3983bb10e1
Ben Cox <benjamin.cox@jadedpixel.com>
2014-01-17 13:59:19 -0500

    Fixing typo........

--- test/fulfillment_service_test.rb
+++ test/fulfillment_service_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class FulFillmentServiceTest < Test::Unit::TestCase
 	test 'new should create fulfillment service' do
-		fake "fulfillment_service", :method => :post
+		fake "fulfillment_services", :method => :post, :body => load_fixture('fulfillment_service')
 		fulfillment_service = ShopifyAPI::FulfillmentService.new(:name => "SomeService")
 		fulfillment_service.save
 		assert_equal "SomeService" , fulfillment_service.name
