ed374a63e9953ee5cb3d9250fa017bd919dd29d7
Peter Schröder <phoetmail@googlemail.com>
2014-01-21 09:33:37 -0500

    reformat

--- lib/active_resource/json_errors.rb
+++ lib/active_resource/json_errors.rb
@@ -2,6 +2,16 @@ require 'active_resource/base'
 
 module ActiveResource
   class Errors < ActiveModel::Errors
+    def from_json(json, save_cache = false)
+      data = ActiveSupport::JSON.decode(json)['errors'] || {} rescue {}
+      case data
+      when String
+        from_string(data, save_cache)
+      else
+        from_hash data, save_cache
+      end
+    end
+
     def from_hash(messages, save_cache = false)
       clear unless save_cache
 
@@ -16,16 +26,6 @@ module ActiveResource
       clear unless save_cache
 
       add("message", error)
-    end
-
-    def from_json(json, save_cache = false)
-      data = ActiveSupport::JSON.decode(json)['errors'] || {} rescue {}
-      case data
-      when String
-        from_string(data, save_cache)
-      else
-        from_hash data, save_cache
-      end
     end
   end
 end
