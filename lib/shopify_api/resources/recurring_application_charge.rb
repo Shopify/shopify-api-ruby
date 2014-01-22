23ebd1cd0e3a415a57354104fac110dc663846ed
Eric Ponce <tricokun@gmail.com>
2014-01-22 12:23:55 +0100

    recurring application tests

--- lib/shopify_api/resources/recurring_application_charge.rb
+++ lib/shopify_api/resources/recurring_application_charge.rb
@@ -1,7 +1,7 @@
 module ShopifyAPI
   class RecurringApplicationCharge < Base
     undef_method :test
-    
+
     class << self
       def current
         all.find { |c| c.status == 'active' }
@@ -11,11 +11,11 @@ module ShopifyAPI
         define_method(status) { all.select { |c| c.status == status.to_s } }
       end
     end
-    
+
     def cancel
       load_attributes_from_response(self.destroy)
     end
-    
+
     def activate
       load_attributes_from_response(post(:activate))
     end
