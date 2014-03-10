7d9cd5eee429fcb6a091d9e70d996d0c21264018
Ben Cox <benjamin.cox@jadedpixel.com>
2014-03-10 15:05:07 -0400

    Adding post and put tests for script tags

--- test/script_tag_test.rb
+++ test/script_tag_test.rb
@@ -12,4 +12,19 @@ class ScriptTagTest < Test::Unit::TestCase
     script_tag = ShopifyAPI::ScriptTag.find(421379493)
     assert_equal "http://js-aplenty.com/bar.js", script_tag.src
   end
+
+  test "create should create a new script tag" do
+    fake 'script_tags', :method => :post, :status => 201, :body => load_fixture('script_tag')
+    script_tag = ShopifyAPI::ScriptTag.create(event: "onload", src: "http://js-aplenty.com/bar.js")
+    assert_equal "http://js-aplenty.com/bar.js", script_tag.src
+  end
+
+  test "editing script tag should update script tag" do
+    fake 'script_tags/421379493', :method => :get, :status => 200, :body => load_fixture('script_tag')
+    script_tag = ShopifyAPI::ScriptTag.find(421379493)
+    script_tag.src = "http://js-aplenty.com/bar.js"
+    fake 'script_tags/421379493', :method => :put, :status => 200, :body => load_fixture('script_tag')
+    script_tag.save
+    assert_equal "http://js-aplenty.com/bar.js", script_tag.src
+  end
 end
