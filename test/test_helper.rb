215a7f97f341ed688474f46b9bbdcc6e8629c401
Ben Cox <ben@shopify.com>
2014-11-13 10:33:02 -0500

    Remove debugger

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,6 @@ require 'rubygems'
 require 'test/unit'
 require 'fakeweb'
 require 'mocha/setup'
-require 'debugger'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
