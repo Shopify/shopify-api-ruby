3717b6901c30f75a5446ec84b5cb3293768a017b
Christian Joudrey <cmallette@gmail.com>
2015-01-19 20:43:37 +0000

    Add GiftCard resource

---
+++ lib/shopify_api/resources/gift_card.rb
@@ -0,0 +1,7 @@
+module ShopifyAPI
+  class GiftCard < Base
+    def disable
+      load_attributes_from_response(post(:disable))
+    end
+  end
+end
---
+++ test/gift_card_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class GiftCardTest < Test::Unit::TestCase
+  def setup
+    super
+    load_gift_card
+  end
+
+  def test_disable
+    fake 'gift_cards/1023670/disable.json', :body => load_fixture('gift_card_disabled'), :method => :post, :extension => false
+
+    refute @gift_card.disabled_at
+    @gift_card.disable
+    assert @gift_card.disabled_at
+  end
+
+  private
+  def load_gift_card
+    fake 'gift_cards/1023670', :body => load_fixture('gift_card')
+    @gift_card = ShopifyAPI::GiftCard.find(1023670)
+  end
+end
