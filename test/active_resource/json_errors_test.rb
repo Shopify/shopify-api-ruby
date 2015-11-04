be8a99b6ac249352dd2b3d6a094275ac05d1ae7f
Shawn French <shawn.french@jadedpixel.com>
2015-11-04 01:47:06 +0000

    Fixup json parse tests

--- test/active_resource/json_errors_test.rb
+++ test/active_resource/json_errors_test.rb
@@ -4,19 +4,16 @@ module ActiveResource
   class JsonErrorsTest < Test::Unit::TestCase
 
     def test_parsing_of_error_json_hash
-      errors = some_error.from_json({errors: {name: ['missing']}}.to_json)
-      assert_equal({"name"=>["missing"]}, errors)
+      @model = ShopifyAPI::Order.new
+      @model.errors.from_json({errors: {name: ['missing']}}.to_json)
+      assert_equal ['missing'], @model.errors[:name]
     end
 
     def test_parsing_of_error_json_plain_string
-      errors = some_error.from_json({errors: 'some generic error'}.to_json)
-      assert_equal(["some generic error"], errors)
-    end
-
-    private
-
-    def some_error
-      ActiveResource::Errors.new(ShopifyAPI::Order.new)
+      @model = ShopifyAPI::Order.new
+      @model.errors.from_json({errors: 'some generic error'}.to_json)
+      assert_equal ['some generic error'], @model.errors[:base]
+      assert_equal 'some generic error', @model.errors.full_messages.to_sentence
     end
   end
 end
