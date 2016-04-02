09df94568642f9b739f0aa2660c18462ba98a089
Julio Napurí <julionc@gmail.com>
2016-04-02 13:16:27 -0500

    Add Custom Collection tests

---
+++ test/custom_collection_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CustomCollectionTest < Test::Unit::TestCase
+  test "#create should create a custom collection" do
+    fake "custom_collections", :method => :post, :status => 201, :body => load_fixture('custom_collection')
+    link = ShopifyAPI::CustomCollection.create(:title => "Macbooks", :image => { :src => "http://example.com/rails_logo.gif" })
+    assert_equal 1063001463, link.id
+  end
+end
