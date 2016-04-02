f770b2de5f6ec3a49142dbb86891cb991adfe1c4
Julio Napurí <julionc@gmail.com>
2016-04-02 13:15:16 -0500

    Add Collect tests

---
+++ test/collect_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CollectTest < Test::Unit::TestCase
+  test "#create should create a collect" do
+    fake "collects", :method => :post, :status => 201, :body => load_fixture('collect')
+    link = ShopifyAPI::Collect.create(:product_id => 921728736, :collection_id => 841564295)
+    assert_equal 841564295, link.id
+  end
+end
