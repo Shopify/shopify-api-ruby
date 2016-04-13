d498663780dcb28d3210a48ade8d94b32b199ba7
Gareth du Plooy <gduplooy@gmail.com>
2016-04-13 11:38:38 -0500

    Adds ability to open a fulfillment.

--- test/fulfillment_test.rb
+++ test/fulfillment_test.rb
@@ -34,6 +34,20 @@ class FulFillmentTest < Test::Unit::TestCase
       end
     end
 
+    context "#open" do
+      should "be able to open a fulfillment" do
+        fulfillment = ShopifyAPI::Fulfillment.find(255858046, :params => {:order_id => 450789469})
+
+        open_fulfillment = ActiveSupport::JSON.decode(load_fixture('fulfillment'))
+        open_fulfillment['fulfillment']['status'] = 'open'
+        fake "orders/450789469/fulfillments/255858046/open", :method => :post, :body => ActiveSupport::JSON.encode(open_fulfillment)
+
+        assert_equal 'pending', fulfillment.status
+        assert fulfillment.open
+        assert_equal 'open', fulfillment.status
+      end
+    end
+
     context "#find" do
       should "be able to find fulfillment" do
         fulfillment = ShopifyAPI::Fulfillment.find(255858046, :params => {:order_id => 450789469})
