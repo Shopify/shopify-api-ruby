c80483889648449234b253c53d90495ee08d419b
Julio Napurí <julionc@gmail.com>
2016-04-18 19:13:52 -0500

    Add Customer#account_activation_url method

--- lib/shopify_api/resources/customer.rb
+++ lib/shopify_api/resources/customer.rb
@@ -9,5 +9,16 @@ module ShopifyAPI
     def self.search(params)
       find(:all, from: :search, params: params)
     end
+
+    def account_activation_url
+      resource = post(:account_activation_url, {}, only_id)
+      data = ActiveSupport::JSON.decode(resource.body.to_s)
+      result = nil
+
+      if data.key?('account_activation_url')
+        result = data['account_activation_url']
+      end
+      result
+    end
   end
 end
--- test/customer_test.rb
+++ test/customer_test.rb
@@ -1,10 +1,20 @@
 require 'test_helper'
 
 class CustomerTest < Test::Unit::TestCase
-  def test_search
-    fake "customers/search.json?query=Bob+country%3AUnited+States", extension: false, body: load_fixture('customers_search')
+
+  test "search should get a customers collection" do
+    fake "customers/search.json?query=Bob+country%3AUnited+States", :extension => false, :body => load_fixture('customers_search')
 
     results = ShopifyAPI::Customer.search(query: 'Bob country:United States')
     assert_equal 'Bob', results.first.first_name
+  end
+
+  test "account_activation_url should create an account activation url" do
+    fake "customers/207119551", :body => load_fixture('customers')
+    fake "customers/207119551/account_activation_url", :method => :post, :body => load_fixture('customers_account_activation_url')
+
+    customer = ShopifyAPI::Customer.find(207119551)
+    activation_url = "http://apple.myshopify.com/account/activate/207119551/86688abf23572680740b1c062fa37111-1458248616"
+    assert_equal activation_url, customer.account_activation_url
   end
 end
