0234cf6e1a4e7d8b60f2ac3cbc2988e5c54b0e01
Francois Chagnon <francois.chagnon@jadedpixel.com>
2016-05-05 12:50:34 -0400

    Add ability to delegate an access token

---
+++ lib/shopify_api/resources/access_token.rb
@@ -0,0 +1,8 @@
+module ShopifyAPI
+  class AccessToken < Base
+    def self.delegate(access_scope, expires_in = nil)
+      resource = post(:delegate, delegate_access_scope: access_scope, expires_in: expires_in)
+      instantiate_record(format.decode(resource.body), {})
+    end
+  end
+end
