1232bc407083c6e173b4ac965bd220ec1e525800
Maarten van Grootel <maarten.vangrootel@shopify.com>
2016-06-01 15:30:34 -0400

    Add draft orders resource

---
+++ lib/shopify_api/resources/draft_order.rb
@@ -0,0 +1,13 @@
+module ShopifyAPI
+  class DraftOrder < Base
+    def complete(params)
+      response = put(:complete, {}, params)
+      load_attributes_from_response(response)
+    end
+
+    def send_invoice(params)
+      response = post(:send_invoice, {}, params)
+      load_attributes_from_response(response)
+    end
+  end
+end
