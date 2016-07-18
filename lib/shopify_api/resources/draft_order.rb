aeec8210c3447d2c29e0e5ddbf08aa8cb4293612
Maarten van Grootel <maarten.vangrootel@shopify.com>
2016-07-18 11:50:00 -0400

    Add checkout action

--- lib/shopify_api/resources/draft_order.rb
+++ lib/shopify_api/resources/draft_order.rb
@@ -9,5 +9,10 @@ module ShopifyAPI
       response = post(:send_invoice, {}, params)
       load_attributes_from_response(response)
     end
+
+    def checkout(params = only_id)
+      response = post(:checkout)
+      instantiate_record(format.decode(resource.body), {})
+    end
   end
 end
