a0500520048a4ae6d7afb5b56ea14bd2c0f32051
Matt Scriven <matt.scriven@shopify.com>
2017-01-06 15:08:00 -0500

    Fix tests

--- test/product_listing_test.rb
+++ test/product_listing_test.rb
@@ -3,15 +3,16 @@ require 'test_helper'
 class ProductListingTest < Test::Unit::TestCase
 
   def test_get_product_listings
-    fake "product_listings/999", method: :get, status: 201, body: load_fixture('product_listings')
+    fake "applications/999/product_listings", method: :get, status: 201, body: load_fixture('product_listings')
 
     product_listings = ShopifyAPI::ProductListing.find(:all, params: { application_id: 999})
 
     assert_equal 2, product_listings.count
-    assert_equal 2, product_listings.first.id
-    assert_equal 1, product_listings.last.id
+
+    assert_equal 2, product_listings.first.product_id
+    assert_equal 1, product_listings.last.product_id
     assert_equal 'Synergistic Silk Chair', product_listings.first.title
-    assert_equal 'Rustic Marble Bottle', product_listings.last.title
+    assert_equal 'Rustic Copper Bottle', product_listings.last.title
   end
 
 end
