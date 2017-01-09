cc4ad46be878633f29d130d4207b373b4736b9ee
Matt Scriven <matt.scriven@shopify.com>
2017-01-09 14:48:30 -0500

    Collection listing product ids

--- lib/shopify_api/resources/collection_listing.rb
+++ lib/shopify_api/resources/collection_listing.rb
@@ -1,5 +1,9 @@
 module ShopifyAPI
   class CollectionListing < Base
     init_prefix :application
+
+    def product_ids(options = {})
+      get("#{collection_id}/product_ids", options[:params])
+    end
   end
 end
--- test/collection_listing_test.rb
+++ test/collection_listing_test.rb
@@ -14,19 +14,12 @@ class CollectionListingTest < Test::Unit::TestCase
 
   def test_get_collection_listing_for_collection_id
     fake "applications/999/collection_listings/1", method: :get, status: 201, body: load_fixture('collection_listing')
+    fake "applications/999/collection_listings//1/product_ids", method: :get, status: 201, body: load_fixture('collection_listing_product_ids')
 
     collection_listing = ShopifyAPI::CollectionListing.find(1, params: { application_id: 999 })
 
+    assert_equal 1, collection_listing.collection_id
     assert_equal 'Home page', collection_listing.title
+    assert_equal [1, 2], collection_listing.product_ids
   end
-
-  # def test_get_collection_listing_product_ids
-  #   fake "applications/999/collection_listings/product_ids", method: :get, status: 201, body: load_fixture('collection_listing_product_ids')
-
-  #   product_ids = ShopifyAPI::CollectionListing.product_ids(params: { application_id: 999 })
-
-  #   assert_equal 2, product_ids.count
-  #   assert_equal 2, product_ids.first
-  #   assert_equal 1, product_ids.last
-  # end
 end
