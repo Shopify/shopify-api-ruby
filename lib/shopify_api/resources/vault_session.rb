22e62eaf75d201acac3c46e13eead2729ee570e6
Maxime Bedard <maxime.bedard@shopify.com>
2017-01-09 13:34:24 -0500

    add comments

--- lib/shopify_api/resources/vault_session.rb
+++ lib/shopify_api/resources/vault_session.rb
@@ -1,4 +1,6 @@
 module ShopifyAPI
+  # avoid removing root_key as it activeresource object initialization
+  # https://github.com/rails/activeresource/blob/master/lib/active_resource/formats/json_format.rb#L21
   module VaultSessionFormat
     extend self
 
