14de7cf842ee8d40058dc654a7a11313ae8389a8
Matt Scriven <matt.scriven@shopify.com>
2017-01-12 10:49:29 -0500

    Test

--- test/product_listing_test.rb
+++ test/product_listing_test.rb
@@ -6,7 +6,6 @@ class ProductListingTest < Test::Unit::TestCase
     fake "applications/999/product_listings", method: :get, status: 201, body: load_fixture('product_listings')
 
     product_listings = ShopifyAPI::ProductListing.find(:all, params: { application_id: 999 })
-
     assert_equal 2, product_listings.count
     assert_equal 2, product_listings.first.product_id
     assert_equal 1, product_listings.last.product_id
@@ -18,7 +17,6 @@ class ProductListingTest < Test::Unit::TestCase
     fake "applications/999/product_listings/2", method: :get, status: 201, body: load_fixture('product_listing')
 
     product_listing = ShopifyAPI::ProductListing.find(2, params: { application_id: 999 })
-
     assert_equal 'Synergistic Silk Chair', product_listing.title
   end
 
@@ -26,7 +24,6 @@ class ProductListingTest < Test::Unit::TestCase
     fake "applications/999/product_listings/product_ids", method: :get, status: 201, body: load_fixture('product_listing_product_ids')
 
     product_ids = ShopifyAPI::ProductListing.product_ids(params: { application_id: 999 })
-
     assert_equal 2, product_ids.count
     assert_equal 2, product_ids.first
     assert_equal 1, product_ids.last
