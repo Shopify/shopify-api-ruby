fc262e8a33bf22b6fe6865b7363d213dd99356b0
Michael Bianco <iloveitaly@gmail.com>
2017-05-01 11:18:16 -0600

    Adding GiftCardAdjustment

--- lib/shopify_api/resources/gift_card.rb
+++ lib/shopify_api/resources/gift_card.rb
@@ -1,5 +1,9 @@
 module ShopifyAPI
   class GiftCard < Base
+    class Adjustment < Base
+      init_prefix :gift_card
+    end
+
     def disable
       load_attributes_from_response(post(:disable))
     end
