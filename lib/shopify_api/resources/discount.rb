2f085e0110a01d0bc1599cc7b29200360d4fda6f
Felix Descoteaux <flx.descoteaux@gmail.com>
2017-07-17 18:00:13 -0400

    switching to legacy path

--- lib/shopify_api/resources/discount.rb
+++ lib/shopify_api/resources/discount.rb
@@ -1,9 +1,11 @@
 module ShopifyAPI
   class Discount < Base
+    self.element_name = 'legacy_discount'
+
     def disable
       load_attributes_from_response(post(:disable))
     end
-    
+
     def enable
       load_attributes_from_response(post(:enable))
     end
