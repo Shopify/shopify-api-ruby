50b5d93bdbb573edbb7635e835d1a037872e902b
Jamie Dwyer <jamie.dwyer@shopify.com>
2018-06-25 20:20:12 -0400

    Add support for ApiPermissions#destroy

--- lib/shopify_api/resources/o_auth.rb
+++ lib/shopify_api/resources/o_auth.rb
@@ -1,8 +1,16 @@
+# frozen_string_literal: true
+
+# This resource is deprecated and will be removed in a future version of this gem.
+# Use ShopifyAPI::ApiPermission.destroy instead
+
 module ShopifyAPI
   class OAuth < Base
     self.collection_name = 'oauth'
 
     def self.revoke
+      warn '[DEPRECATED] ShopifyAPI::OAuth#revoke is deprecated and will be removed in a future version. ' \
+        'Use ShopifyAPI::ApiPermission#destroy instead.'
+
       delete(:revoke)
     end
   end
