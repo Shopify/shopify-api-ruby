65d4140c7d0876ec2dc0d3872ccb47519fae8f32
David Lawson <hello@davidlawson.com.au>
2018-07-17 08:01:11 +1000

    Add lookup to DiscountCode
    For #403

--- lib/shopify_api/resources/discount_code.rb
+++ lib/shopify_api/resources/discount_code.rb
@@ -5,5 +5,13 @@ module ShopifyAPI
     def price_rule_id
       @prefix_options[:price_rule_id]
     end
+    
+    def self.lookup(code)
+      begin
+        self.find(:one, from: '/admin/discount_codes/lookup.json?code=' + code)
+      rescue ActiveResource::Redirection => ex
+        self.find(:one, from: ex.response['Location'])
+      end
+    end
   end
 end
