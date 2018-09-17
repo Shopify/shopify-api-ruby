7068ce96c2695b9f6f77cd00aae12db727621a36
Emmanuel Milou <emmanuel.milou@shopify.com>
2018-09-17 11:57:23 -0400

    Add payments tests

--- test/checkouts_test.rb
+++ test/checkouts_test.rb
@@ -1,26 +1,38 @@
+# frozen_string_literal: true
 require 'test_helper'
 
 class CheckoutsTest < Test::Unit::TestCase
+  def setup
+    super
+
+    @expected_checkouts = JSON.parse(load_fixture('checkouts'))['checkouts']
+    @expected_checkout_id = JSON.parse(load_fixture('checkout'))['checkout']['token']
+  end
+
   test ":create creates a checkout" do
     fake 'checkouts', method: :post, status: 201, body: load_fixture('checkout')
+
     checkout = ShopifyAPI::Checkout.create
-    assert_equal "2a1ace52255252df566af0faaedfbfa7", checkout.id
+
+    assert_equal @expected_checkout_id, checkout.id
   end
 
   test "get all checkouts indexed by token" do
     fake 'checkouts', method: :get, status: 200, body: load_fixture('checkouts')
+
     checkouts = ShopifyAPI::Checkout.all
-    assert_equal '2a1ace52255252df566af0faaedfbfa7', checkouts.first.id
-    assert_equal JSON.parse(load_fixture('checkouts'))['checkouts'].size, checkouts.size
+
+    assert_equal @expected_checkout_id, checkouts.first.id
+    assert_equal @expected_checkouts.size, checkouts.size
   end
 
   test ":complete completes a checkout" do
-    checkout_id = JSON.parse(load_fixture('checkout'))['checkout']['id']
+    fake "checkouts/#{@expected_checkout_id}", method: :get, status: 200, body: load_fixture('checkout')
 
-    fake "checkouts/#{checkout_id}", method: :get, status: 200, body: load_fixture('checkout')
-    checkout = ShopifyAPI::Checkout.find(checkout_id)
+    checkout = ShopifyAPI::Checkout.find(@expected_checkout_id)
 
-    fake "checkouts/#{checkout.id}/complete", method: :post, status: 200, body: load_fixture('checkouts')
+    fake "checkouts/#{@expected_checkout_id}/complete", method: :post, status: 200, body: load_fixture('checkouts')
+
     checkout.complete
   end
 end
