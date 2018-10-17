5cd5ec5bd8f1982faa15e14f5468198a734bce20
Kris Penney <hello@krispenney.me>
2018-10-17 10:47:25 -0400

    Add test to fetch all transactions
    Fixture update

--- test/tender_transaction_test.rb
+++ test/tender_transaction_test.rb
@@ -5,12 +5,14 @@ require 'test_helper'
 class TenderTransactionTest < Test::Unit::TestCase
   def setup
     super
-    fake "/tender_transactions.json", method: :get, body: load_fixture('tender_transactions')
+    fake "tender_transactions", method: :get, body: load_fixture('tender_transactions')
   end
 
   context "Tender Transaction" do
     should 'return a list of transactions' do
       tender_transactions = ShopifyAPI::TenderTransaction.all
+      assert_equal 3, tender_transactions.length
+      assert_equal [1, 2, 3], tender_transactions.map(&:id)
     end
   end
 end
