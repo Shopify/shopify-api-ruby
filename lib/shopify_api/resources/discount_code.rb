8f284d234d87f1abc91b3796f6c3ffd9aa1d8753
David <davidlawson@nuraphone.com>
2018-11-16 16:43:08 +1100

    discount code - reducing repetition

--- lib/shopify_api/resources/discount_code.rb
+++ lib/shopify_api/resources/discount_code.rb
@@ -9,9 +9,10 @@ module ShopifyAPI
     def self.lookup(code)
       find(:one, from: '/admin/discount_codes/lookup.json?code=' + code)
     rescue ActiveResource::Redirection => ex
-      r = find(:one, from: ex.response['Location'])
+      location = ex.response['Location']
+      r = find(:one, from: location)
       unless r.nil?
-        r.prefix_options[:price_rule_id] = %r{price_rules/([0-9]+)}.match(ex.response['Location'])[1].to_i
+        r.prefix_options[:price_rule_id] = %r{price_rules/([0-9]+)}.match(location)[1].to_i
       end
       r
     end
