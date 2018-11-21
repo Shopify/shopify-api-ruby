989a6f158f9ebf7bcc804be6738256f641b02d0e
Tieshia Francis <tieshia.francis@shopify.com>
2018-11-20 16:41:23 -0800

    Formatting.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -91,6 +91,7 @@ class Test::Unit::TestCase < Minitest::Unit::TestCase
     else
       "https://this-is-my-test-shop.myshopify.com/admin/#{endpoint}#{extension}"
     end
+
     FakeWeb.register_uri(method, url, {:body => body, :status => 200, :content_type => "text/#{format}", :content_length => 1}.merge(options))
   end
 end
