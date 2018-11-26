51dfe9c96389d52d72ff09ba89d1c9c4f8258bbe
Tieshia Francis <tieshia.francis@shopify.com>
2018-11-26 15:53:11 -0800

    Updated gem with delivery confirmations based on whether or not message successfully delivered by integration.

--- lib/shopify_api/resources/ping/message.rb
+++ lib/shopify_api/resources/ping/message.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
+
+require 'byebug'
+
 module ShopifyAPI
   module Ping
     class Message < Base
       self.prefix = "/admin/api/ping-api/v1/conversations/:conversation_id/"
 
-      def confirm(details)
-        delivery_details = ShopifyAPI::Ping::DeliveryConfirmation.new(details)
+      def successful_delivery(prefix_options)
+        delivery_details = ShopifyAPI::Ping::DeliveryConfirmation.new(prefix_options)
+        delivery_details.save
+        "true"
+      end
+
+      def failed_delivery(prefix_options)
+        delivery_details = ShopifyAPI::Ping::DeliveryConfirmation.new(prefix_options)
         delivery_details.save
         delivery_details
       end
--- test/ping/message_test.rb
+++ test/ping/message_test.rb
@@ -19,7 +19,7 @@ class PingMessageTest < Test::Unit::TestCase
     assert_equal "d0c7a2e6-8084-4e79-8483-e4a1352b81f7", message.id
   end
 
-  def test_confirm
+  def test_successful_delivery
     fake "api/ping-api/v1/conversations/123/messages", method: :post, body: load_fixture('ping/message')
 
     message = ShopifyAPI::Ping::Message.new(
@@ -35,10 +35,36 @@ class PingMessageTest < Test::Unit::TestCase
 
     fake "api/ping-api/v1/conversations/123/messages/#{message.id}/delivery_confirmation",
       method: :post,
-      body: load_fixture('ping/delivery_confirmation')
+      body: ''
 
-    delivery_confirmation = message.confirm(conversation_id: '123', message_id: message.id)
+    delivery_confirmation = message.successful_delivery(conversation_id: '123', message_id: message.id)
 
-    assert_equal "true", delivery_confirmation.delivered
+    assert_equal "true", delivery_confirmation
   end
+
+  def test_failed_delivery
+    fake "api/ping-api/v1/conversations/123/messages", method: :post, body: load_fixture('ping/message')
+
+    message = ShopifyAPI::Ping::Message.new(
+      dedupe_key: SecureRandom.uuid,
+      content: {
+        text: "Hello from shopify_api",
+      },
+      sender_id: 'test',
+      conversation_id: '123',
+    )
+
+    message.save
+
+    fake "api/ping-api/v1/conversations/123/messages/#{message.id}/delivery_confirmation",
+      method: :post,
+      body: ''
+
+    prefix_options = { conversation_id: '123', message_id: message.id, details: 'Message failed to be sent by integration.' }
+
+    delivery_confirmation = message.failed_delivery(prefix_options)
+
+    assert_equal 'Message failed to be sent by integration.', delivery_confirmation.details
+  end
+
 end
