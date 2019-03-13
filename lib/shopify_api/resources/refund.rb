4fb78decff3508f0815c167a931cfb3d52ebc687
Alex Aitken <alex.aitken@shopify.com>
2019-03-13 15:55:14 -0400

    Stop refund#calculate from modifying the resource prefix.

--- lib/shopify_api/resources/refund.rb
+++ lib/shopify_api/resources/refund.rb
@@ -4,9 +4,10 @@ module ShopifyAPI
 
     def self.calculate(*args)
       options = { :refund => args[0] }
-      params = options.merge(args[1][:params]) if args[1] && args[1][:params]
-      self.prefix = "/admin/orders/#{params[:order_id]}/"
-      resource = post(:calculate, {}, options.to_json)
+      params = {}
+      params = args[1][:params] if args[1] && args[1][:params]
+
+      resource = post(:calculate, params, options.to_json)
       instantiate_record(format.decode(resource.body), {})
     end
   end
