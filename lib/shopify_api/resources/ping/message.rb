ec40d906a79d95e07ca3bfed720be9a7c0882703
Alex Aitken <alex.aitken@shopify.com>
2019-04-08 16:04:31 -0400

    Remove Ping resource.

--- lib/shopify_api/resources/ping/message.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-module ShopifyAPI
-  module Ping
-    class Message < Base
-      self.resource_prefix = "api/ping-api/v1/conversations/:conversation_id/"
-    end
-  end
-end
