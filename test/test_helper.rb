edcd9f2c9c3aa7157b9083ec6c0938020d14b847
Michael Rokas <michael.rokas@shopify.com>
2021-10-28 13:11:04 -0400

    Fix excessive warnings in tests (#22)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
 # typed: false
 # frozen_string_literal: true
 
-require "minitest/autorun"
-
+$VERBOSE = nil
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+
+require "minitest/autorun"
 require "shopify_api"
