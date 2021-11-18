8e9f9370be5a19175e537114fe6b026bee921eb1
Mohsen <mohs@users.noreply.github.com>
2021-11-18 16:20:02 -0500

    Create session for private apps (#64)

--- test/auth/session_test.rb
+++ test/auth/session_test.rb
@@ -3,7 +3,7 @@
 
 require_relative "../test_helper"
 
-class ContextTest < Minitest::Test
+class SessionTest < Test::Unit::TestCase
   UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
 
   def test_create_session_with_id
@@ -30,5 +30,11 @@ class ContextTest < Minitest::Test
 
     session = ShopifyAPI::Auth::Session.new(id: "id", shop: "test-shop", associated_user: user)
     assert(session.online?)
+  end
+
+  def test_create_session_for_private_app
+    session = ShopifyAPI::Auth::Session.create_for_private_app(shop: "test-shop")
+    assert_equal(ShopifyAPI::Context.api_secret_key, session.access_token)
+    assert_equal(ShopifyAPI::Context.scope, session.scope)
   end
 end
