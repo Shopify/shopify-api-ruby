2361dc472f13dc45f766a86cd743ce2157c81690
Melanie Wang <melanie.wang@shopify.com>
2022-02-24 13:21:10 -0500

    Add error code

--- lib/shopify_api/rest/base_errors.rb
+++ lib/shopify_api/rest/base_errors.rb
@@ -6,17 +6,26 @@ module ShopifyAPI
     class BaseErrors
       extend T::Sig
 
-      sig { returns(T::Array[T.untyped]) }
+      sig { returns(T::Array[ShopifyAPI::Errors::HttpResponseError]) }
       attr_accessor :errors
 
       sig { void }
       def initialize
-        @errors = T.let([], T::Array[Rest::BaseErrors])
+        @errors = T.let([], T::Array[ShopifyAPI::Errors::HttpResponseError])
       end
 
       sig { returns(String) }
       def full_messages
         @errors.join("\n")
+      end
+
+      sig { returns(T::Array[T.untyped]) }
+      def codes
+        codes = []
+        @errors.each do |error|
+          codes << error.code
+        end
+        codes
       end
     end
   end
--- test/rest/base_errors_test.rb
+++ test/rest/base_errors_test.rb
@@ -12,5 +12,13 @@ module ShopifyAPITest
 
       assert_equal(errors.full_messages, "Something went wrong\nAnother thing went wrong")
     end
+
+    def test_outputs_error_code
+      errors = ShopifyAPI::Rest::BaseErrors.new
+      errors.errors << ShopifyAPI::Errors::HttpResponseError.new(code: 404)
+      errors.errors << ShopifyAPI::Errors::HttpResponseError.new(code: 405)
+
+      assert_equal(errors.codes, [404, 405])
+    end
   end
 end
