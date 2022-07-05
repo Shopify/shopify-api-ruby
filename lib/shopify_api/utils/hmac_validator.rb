520d1c0b075f16c7af0ed42a74e1afcf7135f671
Tatsuya Oiwa <tatsuya.oiwa@shopify.com>
2022-07-05 21:08:39 +0900

    Validate HMAC using both old and new API secrets

--- lib/shopify_api/utils/hmac_validator.rb
+++ lib/shopify_api/utils/hmac_validator.rb
@@ -13,18 +13,28 @@ module ShopifyAPI
         def validate(verifiable_query)
           return false unless verifiable_query.hmac
 
-          received_signature = verifiable_query.hmac
-          computed_signature = compute_signature(verifiable_query.to_signable_string)
-          OpenSSL.secure_compare(computed_signature, received_signature)
+          result = validate_signature(verifiable_query, Context.api_secret_key)
+          if result || Context.old_api_secret_key.blank?
+            result
+          else
+            validate_signature(verifiable_query, T.must(Context.old_api_secret_key))
+          end
         end
 
         private
 
-        sig { params(signable_string: String).returns(String) }
-        def compute_signature(signable_string)
+        sig { params(verifiable_query: VerifiableQuery, secret: String).returns(T::Boolean) }
+        def validate_signature(verifiable_query, secret)
+          received_signature = verifiable_query.hmac
+          computed_signature = compute_signature(verifiable_query.to_signable_string, secret)
+          OpenSSL.secure_compare(computed_signature, received_signature)
+        end
+
+        sig { params(signable_string: String, secret: String).returns(String) }
+        def compute_signature(signable_string, secret)
           OpenSSL::HMAC.hexdigest(
             OpenSSL::Digest.new("sha256"),
-            ShopifyAPI::Context.api_secret_key,
+            secret,
             signable_string,
           )
         end
