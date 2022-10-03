6fe3925a6c913985465fcde7b0b9179f3fe88d73
Paulo Margarido <64600052+paulomarg@users.noreply.github.com>
2022-10-03 16:09:56 -0400

    Rotating Admin API versions to match release

--- lib/shopify_api/admin_versions.rb
+++ lib/shopify_api/admin_versions.rb
@@ -5,13 +5,13 @@ module ShopifyAPI
   module AdminVersions
     SUPPORTED_ADMIN_VERSIONS = T.let([
       "unstable",
+      "2022-10",
       "2022-07",
       "2022-04",
       "2022-01",
-      "2021-10",
     ], T::Array[String])
 
-    LATEST_SUPPORTED_ADMIN_VERSION = T.let("2022-07", String)
+    LATEST_SUPPORTED_ADMIN_VERSION = T.let("2022-10", String)
   end
 
   SUPPORTED_ADMIN_VERSIONS = ShopifyAPI::AdminVersions::SUPPORTED_ADMIN_VERSIONS
