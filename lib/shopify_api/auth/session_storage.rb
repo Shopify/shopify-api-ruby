e4e175476d1727c6709dc8440736e0de6b5f8a04
Nelson Wittwer <nelson.wittwer@shopify.com>
2022-11-15 13:49:22 -0500

    logging

--- lib/shopify_api/auth/session_storage.rb
+++ lib/shopify_api/auth/session_storage.rb
@@ -15,7 +15,7 @@ module ShopifyAPI
           .returns(T::Boolean)
       end
       def store_session(session)
-        
+        ::ShopifyAPI::Context.logger.warn("ShopifyAPI::Auth::SessionStorage has been deprecated. The ShopifyAPI  will no longer have responsibility for session persistence. Consider using the `shopify_app` gem which now owns this responsiblity.")
       end
 
       sig do
@@ -23,7 +23,7 @@ module ShopifyAPI
           .returns(T.nilable(Session))
       end
       def load_session(id)
-        
+        ::ShopifyAPI::Context.logger.warn("ShopifyAPI::Auth::SessionStorage has been deprecated. The ShopifyAPI  will no longer have responsibility for session persistence. Consider using the `shopify_app` gem which now owns this responsiblity.")
       end
 
       sig do
@@ -31,9 +31,10 @@ module ShopifyAPI
           .returns(T::Boolean)
       end
       def delete_session(id)
-        
+        ::ShopifyAPI::Context.logger.warn("ShopifyAPI::Auth::SessionStorage has been deprecated. The ShopifyAPI  will no longer have responsibility for session persistence. Consider using the `shopify_app` gem which now owns this responsiblity.")
       end
     end
   end
 end
+
 # rubocop:enable Layout/LineLength
