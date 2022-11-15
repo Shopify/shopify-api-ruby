1d989aa4cdfdf89e09a5626b2cb30ac87d24c05c
Nelson Wittwer <nelson.wittwer@shopify.com>
2022-11-15 15:28:51 -0500

    cleaner deprecation strategy

--- lib/shopify_api/auth/session_storage.rb
+++ lib/shopify_api/auth/session_storage.rb
@@ -1,8 +1,6 @@
 # typed: strict
 # frozen_string_literal: true
 
-# rubocop:disable Layout/LineLength
-
 module ShopifyAPI
   module Auth
     module SessionStorage
@@ -14,27 +12,19 @@ module ShopifyAPI
         abstract.params(session: Session)
           .returns(T::Boolean)
       end
-      def store_session(session)
-        ::ShopifyAPI::Context.logger.warn("ShopifyAPI::Auth::SessionStorage has been deprecated. The ShopifyAPI  will no longer have responsibility for session persistence. Consider using the `shopify_app` gem which now owns this responsiblity.")
-      end
+      def store_session(session); end
 
       sig do
         abstract.params(id: String)
           .returns(T.nilable(Session))
       end
-      def load_session(id)
-        ::ShopifyAPI::Context.logger.warn("ShopifyAPI::Auth::SessionStorage has been deprecated. The ShopifyAPI  will no longer have responsibility for session persistence. Consider using the `shopify_app` gem which now owns this responsiblity.")
-      end
+      def load_session(id); end
 
       sig do
         abstract.params(id: String)
           .returns(T::Boolean)
       end
-      def delete_session(id)
-        ::ShopifyAPI::Context.logger.warn("ShopifyAPI::Auth::SessionStorage has been deprecated. The ShopifyAPI  will no longer have responsibility for session persistence. Consider using the `shopify_app` gem which now owns this responsiblity.")
-      end
+      def delete_session(id); end
     end
   end
 end
-
-# rubocop:enable Layout/LineLength
