3466235fae6938d3696e7464d23b24b6874d8e45
Zoey Lan <zoey.lan@shopify.com>
2023-11-07 10:56:06 -0700

    Add comments

--- lib/shopify_api/clients/graphql/response.rb
+++ lib/shopify_api/clients/graphql/response.rb
@@ -15,6 +15,8 @@ module ShopifyAPI
           super(code: response.code, headers: response.headers, body: response.body)
           # TODO #GRAPHQL_TODO
           #  This is a hack to get the parse the response data into object from graphql-client gem
+          #  We'll need to document how to use this new data object with examples
+          #  and explain that if quries are made with `bypass_schema_check: true` then this will be nil
           @data = query_definition&.new(body["data"])
         end
       end
