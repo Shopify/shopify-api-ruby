e95263aa5ef9bea04c7a721333a1fcbd7a89bb8e
João Fagundes <joaovictordmf@gmail.com>
2025-10-14 11:21:33 -0300

    add new version support to admin_versions file

--- lib/shopify_api/admin_versions.rb
+++ lib/shopify_api/admin_versions.rb
@@ -5,6 +5,7 @@ module ShopifyAPI
   module AdminVersions
     SUPPORTED_ADMIN_VERSIONS = T.let([
       "unstable",
+      "2026-01",
       "2025-10",
       "2025-07",
       "2025-04",
@@ -23,8 +24,8 @@ module ShopifyAPI
       "2022-01",
     ], T::Array[String])
 
-    LATEST_SUPPORTED_ADMIN_VERSION = T.let("2025-07", String)
-    RELEASE_CANDIDATE_ADMIN_VERSION = T.let("2025-10", String)
+    LATEST_SUPPORTED_ADMIN_VERSION = T.let("2025-10", String)
+    RELEASE_CANDIDATE_ADMIN_VERSION = T.let("2026-01", String)
   end
 
   SUPPORTED_ADMIN_VERSIONS = ShopifyAPI::AdminVersions::SUPPORTED_ADMIN_VERSIONS
