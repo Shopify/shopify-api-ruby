0bceb14197a60557db02560305a546c6eca9cdd3
Zoey Lan <zoey.lan@shopify.com>
2025-11-06 16:28:14 -0700

    Add refresh_token and refresh_token_expires_in to AccessTokenResponse

--- lib/shopify_api/auth/oauth/access_token_response.rb
+++ lib/shopify_api/auth/oauth/access_token_response.rb
@@ -13,6 +13,8 @@ module ShopifyAPI
         const :expires_in, T.nilable(Integer)
         const :associated_user, T.nilable(AssociatedUser)
         const :associated_user_scope, T.nilable(String)
+        const :refresh_token, T.nilable(String)
+        const :refresh_token_expires_in, T.nilable(Integer)
 
         sig { returns(T::Boolean) }
         def online_token?
@@ -29,7 +31,9 @@ module ShopifyAPI
             session == other.session &&
             expires_in == other.expires_in &&
             associated_user == other.associated_user &&
-            associated_user_scope == other.associated_user_scope
+            associated_user_scope == other.associated_user_scope &&
+            refresh_token == other.refresh_token &&
+            refresh_token_expires_in == other.refresh_token_expires_in
         end
       end
     end
--- test/auth/oauth/access_token_response_test.rb
+++ test/auth/oauth/access_token_response_test.rb
@@ -37,6 +37,214 @@ module ShopifyAPITest
 
           assert(token_response.online_token?)
         end
+
+        def test_equality_with_all_fields_matching
+          access_token = "access_token"
+          scope = "scope1, scope2"
+          session = "session_id"
+          expires_in = 3600
+          associated_user = ShopifyAPI::Auth::AssociatedUser.new(
+            id: 902541635,
+            first_name: "first",
+            last_name: "last",
+            email: "firstlast@example.com",
+            email_verified: true,
+            account_owner: true,
+            locale: "en",
+            collaborator: false,
+          )
+          associated_user_scope = "user_scope"
+          refresh_token = "refresh_token_value"
+          refresh_token_expires_in = 2000
+
+          token_response1 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            session:,
+            expires_in:,
+            associated_user:,
+            associated_user_scope:,
+            refresh_token:,
+            refresh_token_expires_in:,
+          )
+          token_response2 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            session:,
+            expires_in:,
+            associated_user:,
+            associated_user_scope:,
+            refresh_token:,
+            refresh_token_expires_in:,
+          )
+
+          assert_equal(token_response1, token_response2)
+        end
+
+        def test_inequality_with_different_access_token
+          scope = "scope1, scope2"
+
+          token_response1 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token: "access_token_1",
+            scope:,
+          )
+          token_response2 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token: "access_token_2",
+            scope:,
+          )
+
+          refute_equal(token_response1, token_response2)
+        end
+
+        def test_inequality_with_different_scope
+          access_token = "access_token"
+
+          token_response1 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope: "scope1, scope2",
+          )
+          token_response2 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope: "scope3, scope4",
+          )
+
+          refute_equal(token_response1, token_response2)
+        end
+
+        def test_inequality_with_different_session
+          access_token = "access_token"
+          scope = "scope1, scope2"
+
+          token_response1 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            session: "session_1",
+          )
+          token_response2 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            session: "session_2",
+          )
+
+          refute_equal(token_response1, token_response2)
+        end
+
+        def test_inequality_with_different_expires_in
+          access_token = "access_token"
+          scope = "scope1, scope2"
+
+          token_response1 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            expires_in: 3600,
+          )
+          token_response2 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            expires_in: 7200,
+          )
+
+          refute_equal(token_response1, token_response2)
+        end
+
+        def test_inequality_with_different_associated_user
+          access_token = "access_token"
+          scope = "scope1, scope2"
+          associated_user1 = ShopifyAPI::Auth::AssociatedUser.new(
+            id: 1,
+            first_name: "first",
+            last_name: "last",
+            email: "firstlast@example.com",
+            email_verified: true,
+            account_owner: true,
+            locale: "en",
+            collaborator: false,
+          )
+          associated_user2 = ShopifyAPI::Auth::AssociatedUser.new(
+            id: 2,
+            first_name: "other",
+            last_name: "user",
+            email: "otheruser@example.com",
+            email_verified: true,
+            account_owner: false,
+            locale: "en",
+            collaborator: true,
+          )
+
+          token_response1 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            associated_user: associated_user1,
+          )
+          token_response2 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            associated_user: associated_user2,
+          )
+
+          refute_equal(token_response1, token_response2)
+        end
+
+        def test_inequality_with_different_associated_user_scope
+          access_token = "access_token"
+          scope = "scope1, scope2"
+
+          token_response1 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            associated_user_scope: "user_scope_1",
+          )
+          token_response2 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            associated_user_scope: "user_scope_2",
+          )
+
+          refute_equal(token_response1, token_response2)
+        end
+
+        def test_refresh_token_can_be_set
+          token_response = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token: "token",
+            scope: "scope1, scope2",
+            expires_in: 1000,
+            refresh_token: "refresh_token_value",
+            refresh_token_expires_in: 2000,
+          )
+
+          assert_equal("refresh_token_value", token_response.refresh_token)
+          assert_equal(2000, token_response.refresh_token_expires_in)
+        end
+
+        def test_inequality_with_different_refresh_token
+          access_token = "access_token"
+          scope = "scope1, scope2"
+
+          token_response1 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(access_token:, scope:,
+            refresh_token: "refresh_token_1")
+          token_response2 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(access_token:, scope:,
+            refresh_token: "refresh_token_2")
+
+          refute_equal(token_response1, token_response2)
+        end
+
+        def test_inequality_with_different_refresh_token_expires_in
+          access_token = "access_token"
+          scope = "scope1, scope2"
+
+          token_response1 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            refresh_token_expires_in: 123,
+          )
+          token_response2 = ShopifyAPI::Auth::Oauth::AccessTokenResponse.new(
+            access_token:,
+            scope:,
+            refresh_token_expires_in: 321,
+          )
+
+          refute_equal(token_response1, token_response2)
+        end
       end
     end
   end
