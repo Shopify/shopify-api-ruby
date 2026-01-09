76e8544d9c1a8f9f14b25abdec5a94afb7f17236
Liz Kenyon <elizabeth.kenyon@shopify.com>
2026-01-09 10:06:20 -0600

    Add 2026-01 REST resources

---
+++ test/rest/2026_01/recurring_application_charge_test.rb
@@ -0,0 +1,332 @@
+# typed: strict
+# frozen_string_literal: true
+
+########################################################################################################################
+# This file is auto-generated. If you have an issue, please create a GitHub issue.                                     #
+########################################################################################################################
+
+$VERBOSE = nil
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+
+require "minitest/autorun"
+require "webmock/minitest"
+
+require "shopify_api"
+require_relative "../../test_helper"
+
+class RecurringApplicationCharge202601Test < Test::Unit::TestCase
+  def setup
+    super
+
+    test_session = ShopifyAPI::Auth::Session.new(id: "id", shop: "test-shop.myshopify.io", access_token: "this_is_a_test_token")
+    ShopifyAPI::Context.activate_session(test_session)
+    modify_context(api_version: "2026-01")
+  end
+
+  def teardown
+    super
+
+    ShopifyAPI::Context.deactivate_session
+  end
+
+  sig do
+    void
+  end
+  def test_1()
+    stub_request(:post, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json")
+      .with(
+        headers: {"X-Shopify-Access-Token"=>"this_is_a_test_token", "Accept"=>"application/json", "Content-Type"=>"application/json"},
+        body: { "recurring_application_charge" => hash_including({"name" => "Super Duper Plan", "price" => 10.0, "return_url" => "http://super-duper.shopifyapps.com", "trial_days" => 5}) }
+      )
+      .to_return(status: 200, body: JSON.generate({"recurring_application_charge" => {"id" => 1029266951, "name" => "Super Duper Plan", "price" => "10.00", "billing_on" => nil, "status" => "pending", "created_at" => "2025-07-02T11:10:31-05:00", "updated_at" => "2025-07-02T11:10:31-05:00", "activated_on" => nil, "return_url" => "http://super-duper.shopifyapps.com/", "test" => nil, "cancelled_on" => nil, "trial_days" => 5, "trial_ends_on" => nil, "api_client_id" => 755357713, "decorated_return_url" => "http://super-duper.shopifyapps.com/?charge_id=1029266951", "confirmation_url" => "https://jsmith.myshopify.com/admin/charges/755357713/1029266951/RecurringApplicationCharge/confirm_recurring_application_charge?signature=BAh7BzoHaWRpBAdeWT06EmF1dG9fYWN0aXZhdGVU--5d3422b29fc518b770f1dc29f33408b2702fd244", "currency" => "USD"}}), headers: {})
+
+    response = recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new
+    recurring_application_charge.name = "Super Duper Plan"
+    recurring_application_charge.price = 10.0
+    recurring_application_charge.return_url = "http://super-duper.shopifyapps.com"
+    recurring_application_charge.trial_days = 5
+    recurring_application_charge.save
+
+    assert_requested(:post, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json")
+
+    response = response.first if response.respond_to?(:first)
+
+    # Assert attributes are correctly typed preventing Sorbet errors downstream
+    if response.respond_to?(:original_state)
+      response&.original_state&.each do |key, value|
+        begin
+          response.send(key)
+        rescue TypeError => error
+          fail TypeError.new("#{self.class}##{key} is mistyped: #{error.message}")
+        end
+        response.send(key)
+      end
+    end
+  end
+
+  sig do
+    void
+  end
+  def test_2()
+    stub_request(:post, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json")
+      .with(
+        headers: {"X-Shopify-Access-Token"=>"this_is_a_test_token", "Accept"=>"application/json", "Content-Type"=>"application/json"},
+        body: { "recurring_application_charge" => hash_including({"name" => "Super Duper Plan", "price" => 10.0, "return_url" => "http://super-duper.shopifyapps.com", "capped_amount" => 100, "terms" => "$1 for 1000 emails"}) }
+      )
+      .to_return(status: 200, body: JSON.generate({"recurring_application_charge" => {"id" => 1029266950, "name" => "Super Duper Plan", "price" => "10.00", "billing_on" => nil, "status" => "pending", "created_at" => "2025-07-02T11:10:30-05:00", "updated_at" => "2025-07-02T11:10:30-05:00", "activated_on" => nil, "return_url" => "http://super-duper.shopifyapps.com/", "test" => nil, "cancelled_on" => nil, "trial_days" => 0, "trial_ends_on" => nil, "api_client_id" => 755357713, "decorated_return_url" => "http://super-duper.shopifyapps.com/?charge_id=1029266950", "capped_amount" => "100.00", "balance_used" => 0, "balance_remaining" => "100.00", "risk_level" => 0, "confirmation_url" => "https://jsmith.myshopify.com/admin/charges/755357713/1029266950/RecurringApplicationCharge/confirm_recurring_application_charge?signature=BAh7BzoHaWRpBAZeWT06EmF1dG9fYWN0aXZhdGVU--1a27035cd74cb04125ca75347ba33295072249fd", "currency" => "USD"}}), headers: {})
+
+    response = recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new
+    recurring_application_charge.name = "Super Duper Plan"
+    recurring_application_charge.price = 10.0
+    recurring_application_charge.return_url = "http://super-duper.shopifyapps.com"
+    recurring_application_charge.capped_amount = 100
+    recurring_application_charge.terms = "$1 for 1000 emails"
+    recurring_application_charge.save
+
+    assert_requested(:post, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json")
+
+    response = response.first if response.respond_to?(:first)
+
+    # Assert attributes are correctly typed preventing Sorbet errors downstream
+    if response.respond_to?(:original_state)
+      response&.original_state&.each do |key, value|
+        begin
+          response.send(key)
+        rescue TypeError => error
+          fail TypeError.new("#{self.class}##{key} is mistyped: #{error.message}")
+        end
+        response.send(key)
+      end
+    end
+  end
+
+  sig do
+    void
+  end
+  def test_3()
+    stub_request(:post, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json")
+      .with(
+        headers: {"X-Shopify-Access-Token"=>"this_is_a_test_token", "Accept"=>"application/json", "Content-Type"=>"application/json"},
+        body: { "recurring_application_charge" => hash_including({"name" => "Super Duper Plan", "price" => 10.0, "return_url" => "http://super-duper.shopifyapps.com"}) }
+      )
+      .to_return(status: 200, body: JSON.generate({"recurring_application_charge" => {"id" => 1029266947, "name" => "Super Duper Plan", "price" => "10.00", "billing_on" => nil, "status" => "pending", "created_at" => "2025-07-02T11:10:25-05:00", "updated_at" => "2025-07-02T11:10:25-05:00", "activated_on" => nil, "return_url" => "http://super-duper.shopifyapps.com/", "test" => nil, "cancelled_on" => nil, "trial_days" => 0, "trial_ends_on" => nil, "api_client_id" => 755357713, "decorated_return_url" => "http://super-duper.shopifyapps.com/?charge_id=1029266947", "confirmation_url" => "https://jsmith.myshopify.com/admin/charges/755357713/1029266947/RecurringApplicationCharge/confirm_recurring_application_charge?signature=BAh7BzoHaWRpBANeWT06EmF1dG9fYWN0aXZhdGVU--13dbd717accea352543a04313e8fcd7957c4cde4", "currency" => "USD"}}), headers: {})
+
+    response = recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new
+    recurring_application_charge.name = "Super Duper Plan"
+    recurring_application_charge.price = 10.0
+    recurring_application_charge.return_url = "http://super-duper.shopifyapps.com"
+    recurring_application_charge.save
+
+    assert_requested(:post, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json")
+
+    response = response.first if response.respond_to?(:first)
+
+    # Assert attributes are correctly typed preventing Sorbet errors downstream
+    if response.respond_to?(:original_state)
+      response&.original_state&.each do |key, value|
+        begin
+          response.send(key)
+        rescue TypeError => error
+          fail TypeError.new("#{self.class}##{key} is mistyped: #{error.message}")
+        end
+        response.send(key)
+      end
+    end
+  end
+
+  sig do
+    void
+  end
+  def test_4()
+    stub_request(:post, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json")
+      .with(
+        headers: {"X-Shopify-Access-Token"=>"this_is_a_test_token", "Accept"=>"application/json", "Content-Type"=>"application/json"},
+        body: { "recurring_application_charge" => hash_including({"name" => "Super Duper Plan", "price" => 10.0, "return_url" => "http://super-duper.shopifyapps.com", "test" => true}) }
+      )
+      .to_return(status: 200, body: JSON.generate({"recurring_application_charge" => {"id" => 1029266949, "name" => "Super Duper Plan", "price" => "10.00", "billing_on" => nil, "status" => "pending", "created_at" => "2025-07-02T11:10:29-05:00", "updated_at" => "2025-07-02T11:10:29-05:00", "activated_on" => nil, "return_url" => "http://super-duper.shopifyapps.com/", "test" => true, "cancelled_on" => nil, "trial_days" => 0, "trial_ends_on" => nil, "api_client_id" => 755357713, "decorated_return_url" => "http://super-duper.shopifyapps.com/?charge_id=1029266949", "confirmation_url" => "https://jsmith.myshopify.com/admin/charges/755357713/1029266949/RecurringApplicationCharge/confirm_recurring_application_charge?signature=BAh7BzoHaWRpBAVeWT06EmF1dG9fYWN0aXZhdGVU--1f151a17fb46cdc82cd243f92608fae2885ac0bb", "currency" => "USD"}}), headers: {})
+
+    response = recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new
+    recurring_application_charge.name = "Super Duper Plan"
+    recurring_application_charge.price = 10.0
+    recurring_application_charge.return_url = "http://super-duper.shopifyapps.com"
+    recurring_application_charge.test = true
+    recurring_application_charge.save
+
+    assert_requested(:post, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json")
+
+    response = response.first if response.respond_to?(:first)
+
+    # Assert attributes are correctly typed preventing Sorbet errors downstream
+    if response.respond_to?(:original_state)
+      response&.original_state&.each do |key, value|
+        begin
+          response.send(key)
+        rescue TypeError => error
+          fail TypeError.new("#{self.class}##{key} is mistyped: #{error.message}")
+        end
+        response.send(key)
+      end
+    end
+  end
+
+  sig do
+    void
+  end
+  def test_5()
+    stub_request(:get, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json")
+      .with(
+        headers: {"X-Shopify-Access-Token"=>"this_is_a_test_token", "Accept"=>"application/json"},
+        body: {}
+      )
+      .to_return(status: 200, body: JSON.generate({"recurring_application_charges" => [{"id" => 455696195, "name" => "Super Mega Plan", "price" => "15.00", "billing_on" => "2025-07-02", "status" => "accepted", "created_at" => "2025-07-02T11:09:43-05:00", "updated_at" => "2025-07-02T11:10:34-05:00", "activated_on" => nil, "return_url" => "http://yourapp.example.org", "test" => nil, "cancelled_on" => nil, "trial_days" => 0, "trial_ends_on" => nil, "api_client_id" => 755357713, "decorated_return_url" => "http://yourapp.example.org?charge_id=455696195", "currency" => "USD"}]}), headers: {})
+
+    response = ShopifyAPI::RecurringApplicationCharge.all
+
+    assert_requested(:get, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json")
+
+    response = response.first if response.respond_to?(:first)
+
+    # Assert attributes are correctly typed preventing Sorbet errors downstream
+    if response.respond_to?(:original_state)
+      response&.original_state&.each do |key, value|
+        begin
+          response.send(key)
+        rescue TypeError => error
+          fail TypeError.new("#{self.class}##{key} is mistyped: #{error.message}")
+        end
+        response.send(key)
+      end
+    end
+  end
+
+  sig do
+    void
+  end
+  def test_6()
+    stub_request(:get, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json?since_id=455696195")
+      .with(
+        headers: {"X-Shopify-Access-Token"=>"this_is_a_test_token", "Accept"=>"application/json"},
+        body: {}
+      )
+      .to_return(status: 200, body: JSON.generate({"recurring_application_charges" => [{"id" => 1029266953, "name" => "Super Duper Plan", "price" => "10.00", "billing_on" => nil, "status" => "pending", "created_at" => "2025-07-02T11:10:35-05:00", "updated_at" => "2025-07-02T11:10:35-05:00", "activated_on" => nil, "return_url" => "http://super-duper.shopifyapps.com/", "test" => nil, "cancelled_on" => nil, "trial_days" => 0, "trial_ends_on" => nil, "api_client_id" => 755357713, "decorated_return_url" => "http://super-duper.shopifyapps.com/?charge_id=1029266953", "confirmation_url" => "https://jsmith.myshopify.com/admin/charges/755357713/1029266953/RecurringApplicationCharge/confirm_recurring_application_charge?signature=BAh7BzoHaWRpBAleWT06EmF1dG9fYWN0aXZhdGVU--0395a63fec46f517b12dc65dd7330d14f461e801", "currency" => "USD"}]}), headers: {})
+
+    response = ShopifyAPI::RecurringApplicationCharge.all(
+      since_id: "455696195",
+    )
+
+    assert_requested(:get, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges.json?since_id=455696195")
+
+    response = response.first if response.respond_to?(:first)
+
+    # Assert attributes are correctly typed preventing Sorbet errors downstream
+    if response.respond_to?(:original_state)
+      response&.original_state&.each do |key, value|
+        begin
+          response.send(key)
+        rescue TypeError => error
+          fail TypeError.new("#{self.class}##{key} is mistyped: #{error.message}")
+        end
+        response.send(key)
+      end
+    end
+  end
+
+  sig do
+    void
+  end
+  def test_7()
+    stub_request(:get, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges/455696195.json")
+      .with(
+        headers: {"X-Shopify-Access-Token"=>"this_is_a_test_token", "Accept"=>"application/json"},
+        body: {}
+      )
+      .to_return(status: 200, body: JSON.generate({"recurring_application_charge" => {"id" => 455696195, "name" => "Super Mega Plan", "price" => "15.00", "billing_on" => "2025-07-02", "status" => "pending", "created_at" => "2025-07-02T11:09:43-05:00", "updated_at" => "2025-07-02T11:09:43-05:00", "activated_on" => nil, "return_url" => "http://yourapp.example.org", "test" => nil, "cancelled_on" => nil, "trial_days" => 0, "trial_ends_on" => nil, "api_client_id" => 755357713, "decorated_return_url" => "http://yourapp.example.org?charge_id=455696195", "confirmation_url" => "https://jsmith.myshopify.com/admin/charges/755357713/455696195/RecurringApplicationCharge/confirm_recurring_application_charge?signature=BAh7BzoHaWRpBENfKRs6EmF1dG9fYWN0aXZhdGVU--b5f90d04779cc5242b396e4054f2e650c5dace1c", "currency" => "USD"}}), headers: {})
+
+    response = ShopifyAPI::RecurringApplicationCharge.find(
+      id: 455696195,
+    )
+
+    assert_requested(:get, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges/455696195.json")
+
+    response = response.first if response.respond_to?(:first)
+
+    # Assert attributes are correctly typed preventing Sorbet errors downstream
+    if response.respond_to?(:original_state)
+      response&.original_state&.each do |key, value|
+        begin
+          response.send(key)
+        rescue TypeError => error
+          fail TypeError.new("#{self.class}##{key} is mistyped: #{error.message}")
+        end
+        response.send(key)
+      end
+    end
+  end
+
+  sig do
+    void
+  end
+  def test_8()
+    stub_request(:delete, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges/455696195.json")
+      .with(
+        headers: {"X-Shopify-Access-Token"=>"this_is_a_test_token", "Accept"=>"application/json"},
+        body: {}
+      )
+      .to_return(status: 200, body: JSON.generate({}), headers: {})
+
+    response = ShopifyAPI::RecurringApplicationCharge.delete(
+      id: 455696195,
+    )
+
+    assert_requested(:delete, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges/455696195.json")
+
+    response = response.first if response.respond_to?(:first)
+
+    # Assert attributes are correctly typed preventing Sorbet errors downstream
+    if response.respond_to?(:original_state)
+      response&.original_state&.each do |key, value|
+        begin
+          response.send(key)
+        rescue TypeError => error
+          fail TypeError.new("#{self.class}##{key} is mistyped: #{error.message}")
+        end
+        response.send(key)
+      end
+    end
+  end
+
+  sig do
+    void
+  end
+  def test_9()
+    stub_request(:put, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges/455696195/customize.json?recurring_application_charge%5Bcapped_amount%5D=200")
+      .with(
+        headers: {"X-Shopify-Access-Token"=>"this_is_a_test_token", "Accept"=>"application/json", "Content-Type"=>"application/json"},
+        body: {}
+      )
+      .to_return(status: 200, body: JSON.generate({"recurring_application_charge" => {"id" => 455696195, "name" => "Super Mega Plan", "price" => "15.00", "billing_on" => nil, "status" => "active", "created_at" => "2025-07-02T11:09:43-05:00", "updated_at" => "2025-07-02T11:10:33-05:00", "activated_on" => "2025-07-02", "return_url" => "http://yourapp.example.org", "test" => nil, "cancelled_on" => nil, "trial_days" => 0, "trial_ends_on" => "2025-07-02", "api_client_id" => 755357713, "decorated_return_url" => "http://yourapp.example.org?charge_id=455696195", "capped_amount" => "100.00", "balance_used" => "0.0", "balance_remaining" => "100.00", "risk_level" => 0, "update_capped_amount_url" => "https://jsmith.myshopify.com/admin/charges/755357713/455696195/RecurringApplicationCharge/confirm_update_capped_amount?signature=BAh7BzoHaWRpBENfKRs6EmF1dG9fYWN0aXZhdGVG--0d202ac9e22df0d6d100239ef2ae9ca6ba0a648f", "currency" => "USD"}}), headers: {})
+
+    response = recurring_application_charge = ShopifyAPI::RecurringApplicationCharge.new
+    recurring_application_charge.id = 455696195
+    recurring_application_charge.customize(
+      recurring_application_charge: {"capped_amount" => "200"},
+    )
+
+    assert_requested(:put, "https://test-shop.myshopify.io/admin/api/2026-01/recurring_application_charges/455696195/customize.json?recurring_application_charge%5Bcapped_amount%5D=200")
+
+    response = response.first if response.respond_to?(:first)
+
+    # Assert attributes are correctly typed preventing Sorbet errors downstream
+    if response.respond_to?(:original_state)
+      response&.original_state&.each do |key, value|
+        begin
+          response.send(key)
+        rescue TypeError => error
+          fail TypeError.new("#{self.class}##{key} is mistyped: #{error.message}")
+        end
+        response.send(key)
+      end
+    end
+  end
+
+end
